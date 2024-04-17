e58b8a9d433db53438eb98a929be3209ba949fc7
submission1234 <165913116+submission1234@users.noreply.github.com>
2024-04-17 08:19:19 +0800

    Add files via upload

---
+++ readnpy.py
@@ -0,0 +1,28 @@
+import numpy as np
+dataset = np.load('/home/pmri/WZQ/group-level-emotion-recognition-gcn-master/Features/densenet_test.npz', allow_pickle=True)
+
+# file = np.load('/home/pmri/WZQ/New_try/testnpy/sf/train/X.npy')
+# print(file)
+print(dataset['X'])
+#
+# file1 = np.load('/home/pmri/WZQ/New_try/test_read_npy/audio/X.npy')
+# # print(file1)
+# print(file1.shape)
+#
+# file2 = np.load('/home/pmri/WZQ/New_try/test_read_npy/all/X.npy')
+# # print(file2)
+# print(file2.shape)
+#
+# file3 = np.load('/home/pmri/WZQ/New_try/test_read_npy/all/y.npy')
+# # print(file3)
+# print(file3.shape)
+#
+# file4 = np.load('/home/pmri/WZQ/New_try/test_read_npy/vedio/y.npy')
+# # print(file4)
+# print(file4.shape)
+#
+# file5 = np.load('/home/pmri/WZQ/New_try/test_read_npy/vedio/X.npy')
+# # print(file5)
+# print(file5.shape)
+
+
